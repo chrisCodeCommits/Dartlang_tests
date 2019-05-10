99a96260a7ceeb1663335fbc359ecfd77637e1b0
chris <chrispyprogrammer@gmail.com>
2019-05-10 09:43:23 +0200

    adding body widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('TEST UI KIT'),
         ),
+        body: Card(child: Column(children: <Widget>[],),),
       ),
     );
   }
